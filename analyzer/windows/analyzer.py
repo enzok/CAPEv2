edaa08e09b3de08abaa5dcb78304daa151f731ef
me <7831008+enzok@users.noreply.github.com>
2020-02-13 14:19:44 -0500

    Fix syntax error

--- analyzer/windows/analyzer.py
+++ analyzer/windows/analyzer.py
@@ -1461,7 +1461,7 @@ class CommandPipeHandler(object):
 
         if os.path.exists(file_path):
             #Syntax -> PATH
-            if b"\\memory\\"in file_path:
+            if b"\\memory\\" in file_path:
                 log.info("memory")
                 # aka send this as data for the command
                 self.analyzer.files.dump_file(file_path.decode("utf-8"), category="memory")
@@ -1475,7 +1475,7 @@ class CommandPipeHandler(object):
             log.warning("Received DUMPMEM command with an incorrect argument.")
             return
 
-        dump_memory(int(data))
+        #dump_memory(int(data))
 
     def _handle_dumpreqs(self, data):
         if not data.isdigit():
