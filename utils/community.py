7659ecbe9bc9ab17261cb499fae89881d5ef6584
enzok <7831008+enzok@users.noreply.github.com>
2020-04-19 13:11:51 -0400

    code cleanup

--- utils/community.py
+++ utils/community.py
@@ -7,12 +7,11 @@ import os
 import sys
 
 if sys.version_info[:2] < (3, 6):
-    sys.exit("You are running an incompatible version of Python, please use >= 3.6")
+    sys.exit("You are running an incompatible version of Python,"
+             " please use >= 3.6")
 import logging
-import shutil
 import urllib3
 import argparse
-import tempfile
 import tarfile
 from io import BytesIO
 
@@ -22,6 +21,7 @@ import lib.cuckoo.common.colors as colors
 from lib.cuckoo.common.constants import CUCKOO_ROOT
 log = logging.getLogger(__name__)
 URL = "https://github.com/kevoreilly/community/archive/{0}.tar.gz"
+
 
 def install(enabled, force, rewrite, filepath):
     if filepath and os.path.exists(filepath):
@@ -60,10 +60,12 @@ def install(enabled, force, rewrite, filepath):
             # E.g., "community-master/modules/signatures".
             name_start = "%s/%s" % (directory, outfolder)
             for member in members:
-                if not member.name.startswith(name_start) or name_start == member.name:
+                if not member.name.startswith(name_start) or \
+                        name_start == member.name:
                     continue
 
-                filepath = os.path.join(CUCKOO_ROOT, outfolder, member.name[len(name_start)+1:])
+                filepath = os.path.join(CUCKOO_ROOT, outfolder,
+                                        member.name[len(name_start)+1:])
                 if member.name.endswith(".gitignore"):
                     continue
 
@@ -74,14 +76,16 @@ def install(enabled, force, rewrite, filepath):
 
                 if not rewrite:
                     if os.path.exists(filepath):
-                        print("File \"{0}\" already exists, {1}".format(filepath, colors.yellow("skipped")))
+                        print("File \"{}\" already exists, {}".
+                              format(filepath, colors.yellow("skipped")))
                         continue
 
                 install = False
                 dest_file = os.path.basename(filepath)
                 if not force:
                     while 1:
-                        choice = input("Do you want to install file " "\"{0}\"? [yes/no] ".format(dest_file))
+                        choice = input("Do you want to install file "
+                                       "\"{}\"? [yes/no] ".format(dest_file))
                         if choice.lower() == "yes":
                             install = True
                             break
@@ -96,27 +100,46 @@ def install(enabled, force, rewrite, filepath):
                     if not os.path.exists(os.path.dirname(filepath)):
                         os.makedirs(os.path.dirname(filepath))
 
-                    print("File \"{0}\" {1}".format(filepath, colors.green("installed")))
+                    print("File \"{}\" {}".format(filepath,
+                                                  colors.green("installed")))
                     open(filepath, "wb").write(t.extractfile(member).read())
+
 
 def main():
     global URL
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--all", help="Download everything", action="store_true", required=False)
-    parser.add_argument("-e", "--feeds", help="Download CAPE feed modules", action="store_true", required=False)
-    parser.add_argument("-s", "--signatures", help="Download CAPE signatures", action="store_true", required=False)
-    parser.add_argument("-p", "--processing", help="Download processing modules", action="store_true", required=False)
-    parser.add_argument("-m", "--machinery", help="Download machine managers",action="store_true", required=False)
-    parser.add_argument("-r", "--reporting", help="Download reporting modules", action="store_true", required=False)
-    parser.add_argument("-an", "--analyzer", help="Download analyzer modules/binaries/etc", action="store_true", required=False)
-    parser.add_argument("-data", "--data", help="Download data things", action="store_true", required=False)
-    parser.add_argument("-f", "--force", help="Install files without confirmation", action="store_true", default=False, required=False)
-    parser.add_argument("-w", "--rewrite", help="Rewrite existing files", action="store_true", required=False)
-    parser.add_argument("-b", "--branch", help="Specify a different branch", action="store", default="master", required=False)
-    parser.add_argument("--file", help="Specify a local copy of a community .zip file", action="store", default=False, required=False)
+    parser.add_argument("-a", "--all", help="Download everything",
+                        action="store_true", required=False)
+    parser.add_argument("-e", "--feeds", help="Download CAPE feed modules",
+                        action="store_true", required=False)
+    parser.add_argument("-s", "--signatures", help="Download CAPE signatures",
+                        action="store_true", required=False)
+    parser.add_argument("-p", "--processing",
+                        help="Download processing modules",
+                        action="store_true", required=False)
+    parser.add_argument("-m", "--machinery", help="Download machine managers",
+                        action="store_true", required=False)
+    parser.add_argument("-r", "--reporting", help="Download reporting modules",
+                        action="store_true", required=False)
+    parser.add_argument("-z", "--analyzer",
+                        help="Download analyzer modules/binaries/etc",
+                        action="store_true", required=False)
+    parser.add_argument("-d", "--data", help="Download data items",
+                        action="store_true", required=False)
+    parser.add_argument("-f", "--force",
+                        help="Install files without confirmation",
+                        action="store_true", default=False, required=False)
+    parser.add_argument("-w", "--rewrite", help="Rewrite existing files",
+                        action="store_true", required=False)
+    parser.add_argument("-b", "--branch", help="Specify a different branch",
+                        action="store", default="master", required=False)
+    parser.add_argument("--file",
+                        help="Specify a local copy of a community .zip file",
+                        action="store", default=False, required=False)
     args = parser.parse_args()
 
+    enabled = []
     URL = URL.format(args.branch)
 
     if args.all:
@@ -143,6 +166,7 @@ def main():
         return
 
     install(enabled, args.force, args.rewrite, args.file)
+
 
 if __name__ == "__main__":
     try:
