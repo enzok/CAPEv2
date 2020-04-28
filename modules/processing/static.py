82b7cc054128b687aa4648d74b899cce8fecf842
me <7831008+enzok@users.noreply.github.com>
2020-04-28 11:03:48 -0400

    bug fix

--- modules/processing/static.py
+++ modules/processing/static.py
@@ -1628,8 +1628,8 @@ class Office(object):
                     if decoded_strs:
                         for dstr in decoded_strs.split("\n"):
                             if dstr:
-                                cell, cell_value = dstr.split(" = ")
-                                macrores["Analysis"]["DecodedStrings"].append(cell, convert_to_printable(cell_value))
+                                cell, cell_value = dstr.split(" = ", 1)
+                                macrores["Analysis"]["DecodedStrings"].append((cell, convert_to_printable(cell_value)))
             # Delete and keys which had no results. Otherwise we pollute the
             # Django interface with null data.
             if macrores["Analysis"]["AutoExec"] == []:
