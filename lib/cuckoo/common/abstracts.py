bfb05c6d91232cda9cdf7f90b4797f5aeccd5665
me <7831008+enzok@users.noreply.github.com>
2020-09-16 16:34:16 -0400

    Add back macro yara processing - part 3

--- lib/cuckoo/common/abstracts.py
+++ lib/cuckoo/common/abstracts.py
@@ -793,14 +793,19 @@ class Signature(object):
                                 for sub_block in pe.get(sub_keyword, []) or []:
                                     if re.findall(name, sub_block["name"], re.I):
                                         yield "extracted_pe", pe["path"], sub_block
-        '''
+
         macro_path = os.path.join(CUCKOO_ROOT, "storage", "analyses", str(self.results["info"]["id"]), "macros")
         for macroname in self.results.get("static", {}).get("office", {}).get("Macro", {}).get("info", []) or []:
             for yara_block in self.results["static"]["office"]["Macro"]["info"].get("macroname", []) or []:
                 for sub_block in self.results["static"]["office"]["Macro"]["info"]["macroname"].get(yara_block, []) or []:
                     if re.findall(name, sub_block["name"], re.I):
                         yield "macro", os.path.join(macro_path, macroname), sub_block
-        '''
+
+        if self.results.get("static", {}).get("office", {}).get("XLMMacroDeobfuscator", False):
+            for sub_block in self.results["static"]["office"]["XLMMacroDeobfuscator"].get("info", []).get("yara_macro", []) or []:
+                if re.findall(name, sub_block["name"], re.I):
+                    yield "macro", os.path.join(macro_path, "xlm_macro"), sub_block
+
         yield False, False, False
 
     def add_statistic(self, name, field, value):
