89b5a9743353b2669b735be416eeea51c4a5038a
me <7831008+enzok@users.noreply.github.com>
2020-09-16 14:50:05 -0400

    Remove macro yara processing

--- modules/processing/static.py
+++ modules/processing/static.py
@@ -1397,7 +1397,6 @@ class Office(object):
             # Create IOC and category vars. We do this before processing the
             # macro(s) to avoid overwriting data when there are multiple
             # macros in a single file.
-            macro_folder = os.path.join(CUCKOO_ROOT, "storage", "analyses", str(self.results["info"]["id"]), "macros")
             macrores["Analysis"] = dict()
             macrores["Analysis"]["AutoExec"] = list()
             macrores["Analysis"]["Suspicious"] = list()
@@ -1412,15 +1411,6 @@ class Office(object):
                     macrores["Code"][outputname] = list()
                     macrores["Code"][outputname].append((convert_to_printable(vba_filename), convert_to_printable(vba_code)))
                     autoexec = detect_autoexec(vba_code)
-                    if "Excel 4.0 macro sheet".lower() in vba_code.lower():
-                        if not os.path.exists(macro_folder):
-                            os.makedirs(macro_folder)
-                        macro_file = os.path.join(macro_folder, outputname)
-                        with open(macro_file, "wb") as f:
-                            f.write(vba_code)
-                        macrores["info"][outputname]["yara_macro"] = File(macro_file).get_yara(category="macro")
-                        macrores["info"][outputname]["yara_macro"] = File(macro_file).get_yara(category="CAPE")
-
                     suspicious = detect_suspicious(vba_code)
                     iocs = False
                     try:
@@ -1492,17 +1482,7 @@ class Office(object):
             try:
                 deofuscated_xlm = XLMMacroDeobf(**xlm_kwargs)
                 if deofuscated_xlm:
-                    xlmmacro = results["office"]["XLMMacroDeobfuscator"] = dict()
-                    xlmmacro["Code"] = deofuscated_xlm
-                    macro_folder = os.path.join(CUCKOO_ROOT, "storage", "analyses", str(self.results["info"]["id"]), "macros")
-                    if not os.path.exists(macro_folder):
-                        os.makedirs(macro_folder)
-                    macro_file = os.path.join(macro_folder, "xlm_macro.txt")
-                    with open(macro_file, "wb") as f:
-                        f.write(deofuscated_xlm)
-                    xlmmacro["info"]["path"] = macro_file
-                    xlmmacro["info"]["yara_macro"] = File(macro_file).get_yara(category="macro")
-                    xlmmacro["info"]["yara_macro"] = File(macro_file).get_yara(category="CAPE")
+                    xlmmacro = results["office"]["XLMMacroDeobfuscator"] = deofuscated_xlm
             except Exception as e:
                 log.error(e, exc_info=True)
 
