3467eb9cde2e894fc469977a7ab54f83b654f355
me <7831008+enzok@users.noreply.github.com>
2020-11-04 09:40:29 -0500

    small fix

--- web/analysis/views.py
+++ web/analysis/views.py
@@ -1137,7 +1137,6 @@ def file(request, category, task_id, dlfile):
                 mem_zip = BytesIO()
                 with pyzipper.AESZipFile(mem_zip, 'w', compression=pyzipper.ZIP_LZMA, encryption=pyzipper.WZ_AES) as zf:
                     pwd = settings.ZIP_PWD
-                    zf.setpassword(pwd.encode('utf8'))
                     with open(path, "rb") as f:
                         zf.writestr(os.path.basename(path), f.read())
             else:
