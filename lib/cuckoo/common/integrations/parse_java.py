0563b964797d4c52ee78b2653cb4dd591f6dd22c
me <7831008+enzok@users.noreply.github.com>
2022-02-17 09:50:56 -0500

    Carry over local edits to new static processing lib files

--- lib/cuckoo/common/integrations/parse_java.py
+++ lib/cuckoo/common/integrations/parse_java.py
@@ -14,9 +14,11 @@ log = logging.getLogger(__name__)
 class Java(object):
     """Java Static Analysis"""
 
-    def __init__(self, file_path, decomp_jar):
+    def __init__(self, file_path, decomp_jar, deobfuscator_jar, deobfuscator_conf):
         self.file_path = file_path
         self.decomp_jar = decomp_jar
+        self.deobfuscator_jar = deobfuscator_jar
+        self.deobfuscator_conf = deobfuscator_conf
 
     def run(self):
         """Run analysis.
@@ -28,11 +30,45 @@ class Java(object):
         results = {}
 
         results["java"] = {}
+        ojar_file = ""
+        jar_file = ""
+
+        if self.deobfuscator_jar:
+            f = open(self.file_path, "rb")
+            data = f.read()
+            f.close()
+            # TODO: run with detect: true, then apply from list of approved, discovered transformers
+            try:
+                ijar_file = store_temp_file(data, "obfuscated.jar")
+                tmpdir = os.path.dirname(ijar_file)
+                ojar_file = os.path.join(tmpdir, b"decompile.jar")
+                tmp_conf = os.path.join(tmpdir, b"config.yml")
+                with open (self.deobfuscator_conf, "r") as cf:
+                    confdata = cf.read()
+                with open (tmp_conf, "w") as tf:
+                    tf.write("input: " + ijar_file.decode('utf8') + "\n")
+                    tf.write("output: " + ojar_file.decode('utf8') + "\n")
+                    tf.write(confdata)
+
+                p = Popen(["java", "-jar", self.deobfuscator_jar, "--config", tmp_conf], stdout=PIPE)
+                log.info(convert_to_printable(p.stdout.read()))
+                jar_file = ojar_file
+            except Exception as e:
+                ojar_file = ""
+                log.error(e, exc_info=True)
+                pass
+
+            try:
+                os.unlink(ijar_file)
+            except:
+                pass
 
         if self.decomp_jar:
-            with open(self.file_path, "rb") as f:
+            if not jar_file:
+                f = open(self.file_path, "rb")
                 data = f.read()
-            jar_file = store_temp_file(data, "decompile.jar")
+                f.close()
+                jar_file = store_temp_file(data, "decompile.jar")
 
             try:
                 if self.decomp_jar.endswith(".jar"):
