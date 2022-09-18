e44124ba59b9593dfa9f2205d31b1c5321c59a25
enzok <7831008+enzok@users.noreply.github.com>
2022-09-18 08:52:20 -0400

    Add dll as a service package

--- analyzer/windows/modules/packages/service_dll.py
+++ analyzer/windows/modules/packages/service_dll.py
@@ -1,7 +1,3 @@
-# Copyright (C) 2010-2015 Cuckoo Foundation.
-# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
-# See the file 'docs/LICENSE' for copying permission.
-
 import ctypes
 import logging
 import sys
@@ -10,6 +6,7 @@ from lib.api.process import Process
 from lib.common.abstracts import Package
 from lib.common.common import check_file_extension
 from lib.common.defines import ADVAPI32, KERNEL32
+from winreg import HKEY_LOCAL_MACHINE, KEY_ALL_ACCESS, REG_SZ, REG_EXPAND_SZ, REG_MULTI_SZ, CreateKeyEx, SetValueEx, CloseKey
 
 INJECT_CREATEREMOTETHREAD = 0
 INJECT_QUEUEUSERAPC = 1
@@ -48,28 +45,64 @@ SERVICE_ALL_ACCESS = (
     | SERVICE_USER_DEFINED_CONTROL
 )
 SERVICE_WIN32_OWN_PROCESS = 0x0010
+SERVICE_WIN32_SHARE_PROCESS = 0x0020
 SERVICE_INTERACTIVE_PROCESS = 0x0100
 SERVICE_DEMAND_START = 0x0003
 SERVICE_ERROR_IGNORE = 0x0000
 log = logging.getLogger(__name__)
 
 
-class Service(Package):
-    """Service analysis package."""
+class ServiceDll(Package):
+    """Service Dll analysis package."""
+
+    def __init__(self, options=None, config=None):
+        if options is None:
+            options = {}
+        self.config = config
+        self.options = options
+        self.options["curdir"] = "%SystemRoot%\system32"
 
     PATHS = [
         ("SystemRoot", "system32", "sc.exe"),
     ]
 
+    def set_keys(self, servicename, dllpath):
+
+        svchost_path = r"Software\Microsoft\Windows NT\CurrentVersion\Svchost"
+        parameter_path = rf"System\CurrentControlSet\Services\{servicename}\Parameters"
+
+        try:
+            log.info("Adding Parameters value: %s -> ServiceDll = %s", parameter_path, dllpath)
+            with CreateKeyEx(HKEY_LOCAL_MACHINE, parameter_path, 0, KEY_ALL_ACCESS) as key:
+                SetValueEx(key, "ServiceDll", 0, REG_EXPAND_SZ, dllpath)
+                CloseKey(key)
+        except Exception as e:
+            log.info("Error setting registry value: %s", e)
+            # Service is not installed
+            return
+
+        try:
+            log.info("Adding capegroup value: capegroup = %s", servicename)
+            with CreateKeyEx(HKEY_LOCAL_MACHINE, svchost_path, 0, KEY_ALL_ACCESS) as key:
+                SetValueEx(key, "capegroup", 0, REG_MULTI_SZ, [servicename])
+                CloseKey(key)
+        except Exception as e:
+            log.info("Error setting registry value: %s", e)
+            return
+
     def start(self, path):
         try:
             servicename = self.options.get("servicename", "CAPEService").encode("utf8")
+            if servicename == "blank":
+                servicename = " ".encode("utf8")
             servicedesc = self.options.get("servicedesc", "CAPE Service").encode("utf8")
             arguments = self.options.get("arguments")
-            path = check_file_extension(path, ".exe")
-            binpath = f'"{path}"'.encode("utf8")
+            path = check_file_extension(path, ".dll")
+            svcpath = r"%SystemRoot%\system32\svchost.exe"
+            binpath = f"{svcpath} -k capegroup".encode("utf8")
+            dllpath = f"{path}"
             if arguments:
-                binpath += f" {arguments}"
+                dllpath += f" {arguments}"
             scm_handle = ADVAPI32.OpenSCManagerA(None, None, SC_MANAGER_ALL_ACCESS)
             if scm_handle == 0:
                 log.info("Failed to open SCManager")
@@ -80,7 +113,7 @@ class Service(Package):
                 servicename,
                 servicedesc,
                 SERVICE_ALL_ACCESS,
-                SERVICE_WIN32_OWN_PROCESS,
+                SERVICE_WIN32_SHARE_PROCESS,
                 SERVICE_DEMAND_START,
                 SERVICE_ERROR_IGNORE,
                 binpath,
@@ -94,7 +127,8 @@ class Service(Package):
                 log.info("Failed to create service")
                 log.info(ctypes.FormatError())
                 return
-            log.info("Created service (handle: 0x%s)", service_handle)
+            log.info("Created service %s (handle: 0x%s)", servicename.decode(), service_handle)
+            self.set_keys(servicename.decode(), dllpath)
             servproc = Process(options=self.options, config=self.config, pid=self.config.services_pid, suspended=False)
             filepath = servproc.get_filepath()
             servproc.inject(injectmode=INJECT_QUEUEUSERAPC, interest=filepath, nosleepskip=True)
