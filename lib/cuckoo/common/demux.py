47cc53300490c5d08825942dc5be828f3cb3db73
enzok <7831008+enzok@users.noreply.github.com>
2023-02-27 15:04:07 -0500

    Change quarantine file submission

--- lib/cuckoo/common/demux.py
+++ lib/cuckoo/common/demux.py
@@ -13,6 +13,7 @@ from lib.cuckoo.common.exceptions import CuckooDemuxError
 from lib.cuckoo.common.integrations.parse_pe import HAVE_PEFILE, IsPEImage
 from lib.cuckoo.common.objects import File
 from lib.cuckoo.common.path_utils import path_exists, path_mkdir, path_write_file
+from lib.cuckoo.common.quarantine import unquarantine
 from lib.cuckoo.common.utils import get_options, sanitize_filename, pe_trimmed_size
 
 sf_version = ""
@@ -217,6 +218,11 @@ def demux_sample(filename: bytes, package: str, options: str, use_sflock: bool =
     # if a package was specified, then don't do anything special
     if package:
         return [filename]
+
+    # handle quarantine files
+    tmp_path = unquarantine(filename)
+    if tmp_path:
+        filename = tmp_path
 
     # to handle when side file for exec is required
     if "file=" in options:
