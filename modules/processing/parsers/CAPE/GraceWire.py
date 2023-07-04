3790c27fcf198cdecac6dc304102b143ae6543bd
enzok <7831008+enzok@users.noreply.github.com>
2023-07-04 10:37:23 -0400

    Update parsers for yara

--- modules/processing/parsers/CAPE/GraceWire.py
+++ modules/processing/parsers/CAPE/GraceWire.py
@@ -23,31 +23,31 @@ rule GraceWire {
 """
 
 
-def fix_ips(offsets):
+def fix_ips(hits):
     config_dict = {}
 
     i = -1
     config_dict["Initial C2"] = []
-    for oset, name, ip in offsets:
-        prev_offset = offsets[i][0] + 1
-        if oset == prev_offset:
+    for item in hits:
+        prev_offset = hits[i][0] + 1
+        if item.instances[0].offset == prev_offset:
             i += 1
             continue
-        config_dict["Initial C2"].append(ip)
+        config_dict["Initial C2"].append(item.instances[0].matched_data)
         i += 1
 
     return config_dict
 
 
-def get_password(filebuf, name, offsets):
+def get_password(filebuf, name, hits):
     config_dict = {}
     config_dict[name] = []
 
     pe = pefile.PE(data=filebuf, fast_load=False)
     image_base = pe.OPTIONAL_HEADER.ImageBase
 
-    for oset, bar, foo in offsets:
-        pwdva_offset = unpack_from("i", filebuf, oset + 8)[0] - image_base
+    for item in hits:
+        pwdva_offset = unpack_from("i", filebuf, item.instances[0].offset + 8)[0] - image_base
         pwd_offset = pe.get_offset_from_rva(pwdva_offset)
         password = unpack_from("16c", filebuf, pwd_offset)
         config_dict[name].append("".join(password))
@@ -55,8 +55,8 @@ def get_password(filebuf, name, offsets):
     return config_dict
 
 
-def yara_scan(raw_data, rule_name=None):
-    addresses = []
+def yara_scan(raw_data, rule_name):
+    hits = []
     try:
         yara_rules = yara.compile(source=rule_source)
         matches = yara_rules.match(data=raw_data)
@@ -64,31 +64,29 @@ def yara_scan(raw_data, rule_name=None):
             if match.rule == "GraceWire":
                 for item in match.strings:
                     if rule_name:
-                        if item[1] == rule_name:
-                            addresses.append(item)
-                    else:
-                        addresses.append(item)
+                        if item.identifier.strip("$") == rule_name:
+                            hits.append(item)
     except Exception as e:
         print(e)
 
-    return addresses
+    return hits
 
 
 def config(task_info, data):
-    offsets = {}
-    offsets["dat_password"] = yara_scan(data, rule_name="$dat_password")
-    offsets["bin_password"] = yara_scan(data, rule_name="$bin_password")
-    offsets["ip_address"] = yara_scan(data, rule_name="$ip_address")
-    offsets["version_string"] = yara_scan(data, rule_name="$version_string")
+    hits = {}
+    hits["dat_password"] = yara_scan(data, rule_name="$dat_password")
+    hits["bin_password"] = yara_scan(data, rule_name="$bin_password")
+    hits["ip_address"] = yara_scan(data, rule_name="$ip_address")
+    hits["version_string"] = yara_scan(data, rule_name="$version_string")
 
-    if not offsets:
+    if not hits:
         return
     cfg_dict = {}
-    if offsets["version_string"]:
-        cfg_dict["Version String"] = offsets["version_string"][0][2][9:]
+    if hits["version_string"]:
+        cfg_dict["Version String"] = hits["version_string"][0].instances[0].matched_data[9:]
 
-    cfg_dict.update(fix_ips(offsets["ip_address"]))
-    cfg_dict.update(get_password(data, "dat password", offsets["dat_password"]))
-    cfg_dict.update(get_password(data, "bin password", offsets["bin_password"]))
+    cfg_dict.update(fix_ips(hits["ip_address"]))
+    cfg_dict.update(get_password(data, "dat password", hits["dat_password"]))
+    cfg_dict.update(get_password(data, "bin password", hits["bin_password"]))
 
     return cfg_dict
